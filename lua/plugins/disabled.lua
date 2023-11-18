fa8864fcb9605374d60e27e2df06b57035f4dd07
Oscar Feng <feng.oscar@gmail.com>
2023-11-18 14:04:01 +1100

    trying to figure out eslint issues

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,18 +2,33 @@
 -- Put all plugins you want to disable here
 
 local disabled = {
-  "stevearc/conform.nvim",
+  -- "stevearc/conform.nvim",
+  always = {
+    "stevearc/conform.nvim",
+  },
+
+  vscode = {
+    "lukas-reineke/indent-blankline.nvim",
+    "folke/todo-comments.nvim",
+  },
 }
 
 ----------------------------------------------------------------------------------------------------
 
 local M = {}
 
-for idx, name in pairs(disabled) do
-  M[idx] = {
+for _, name in pairs(disabled.always) do
+  table.insert(M, {
     name,
     enabled = false,
-  }
+  })
+end
+
+for _, name in pairs(disabled.vscode) do
+  table.insert(M, {
+    name,
+    cond = not vim.g.vscode,
+  })
 end
 
 return M
